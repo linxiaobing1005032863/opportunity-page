3c1cab7bed73e18e838cbabf626faea7d20c0e76
itdian <chenjinbode@yeah.net>
2017-08-26 17:11:25 +0800

    daupte

---
+++ js/organize/_base.js
@@ -0,0 +1,16 @@
+//左边点击导航变色
+$('.secOul li').on('click',function () {
+    $(this).addClass('change-svg').siblings().removeClass('change-svg');
+});
+
+//点击中间导航下拉换向 隐藏的ol显示
+$('.secOulTwo li').on('click',function () {
+    console.log(this);
+    $(this).addClass('nav-change').siblings().removeClass('nav-change');
+    $('.secOulTwo li ol').toggle();
+   /* if('nav-change'){
+        $(this).removeClass('nav-change')
+    }else{
+        $(this).addClass('nav-change')
+    }*/
+});
