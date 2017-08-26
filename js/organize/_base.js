754a9530bd3c6e44a507f8136cf3ec0f6040e8e8
itdian <chenjinbode@yeah.net>
2017-08-26 17:45:24 +0800

    update

--- js/organize/_base.js
+++ js/organize/_base.js
@@ -5,12 +5,11 @@ $('.secOul li').on('click',function () {
 
 //点击中间导航下拉换向 隐藏的ol显示
 $('.secOulTwo li').on('click',function () {
-    console.log(this);
-    $(this).addClass('nav-change').siblings().removeClass('nav-change');
-    $('.secOulTwo li ol').toggle();
-   /* if('nav-change'){
-        $(this).removeClass('nav-change')
-    }else{
-        $(this).addClass('nav-change')
-    }*/
+    $(this).toggleClass('nav-change').siblings().removeClass('nav-change');
+    /*$('.secOulTwo li ol').toggle();*/
+    /* if('nav-change'){
+     $(this).removeClass('nav-change')
+     }else{
+     $(this).addClass('nav-change')
+     }*/
 });
